2e6c71700f2467bc1282210fa2efc0ed0d4cc7a2
tafarii <thuntermckenzie@gmail.com>
2016-10-12 16:11:52 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by tafarii on 10/12/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by tafarii on 10/12/16.
+ */
+public class MainTest {
+
+}
